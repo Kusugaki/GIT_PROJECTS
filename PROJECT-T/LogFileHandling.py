53a326daa4e15d2409291c500c56e10c1451506e
Kusugaki <kid.kusu116@gmail.com>
2024-09-09 21:18:50 +0800

    Refactored

--- PROJECT-T/LogFileHandling.py
+++ PROJECT-T/LogFileHandling.py
@@ -3,44 +3,43 @@ import csv
 import os
 
 class FileGetter():
-    fetched_list: list[object] = []
 
     @classmethod
     def fetch_saved_database(cls, path:str) -> list:
+        fetched_list: list[LogEntry] = []
+        
         if not os.path.exists(path):
             print(f"no database found in \'{path}\'.")
             return []
+        
+        try:
+            with open(path, 'r', encoding="utf-8") as csv_file:
+                csv_reader: csv = csv.reader(csv_file)
+                next(csv_reader)
 
-        with open(path, 'r', encoding="utf-8") as csv_file:
-            csv_reader: csv = csv.reader(csv_file)
-
-            line: int = 0
-            for row in csv_reader:
-                if not line == 0:   # SKIP HEADER ROW
-                    cls.get_fetched_list().append(
-                        LogEntry(
-                            total   = row[0],
-                            date    = row[1],
-                            logType = row[2], 
-                            subtype = row[3],
-                            title   = row[4],   
-                            amount  = row[5],
-                            logID   = row[6]
+                for row in csv_reader:
+                        fetched_list.append(
+                            LogEntry(
+                                total   = row[0],
+                                date    = row[1],
+                                logType = row[2], 
+                                subtype = row[3],
+                                title   = row[4],   
+                                amount  = row[5],
+                                logID   = row[6]
+                            )
                         )
-                    )
-                line += 1
+        except Exception as e:
+            print(f"ERROR_READING_FILE \'{path}\': {e}")
 
-        return cls.get_fetched_list()
+        return fetched_list
 
-
-    @classmethod
-    def get_fetched_list(cls):
-        return cls.fetched_list
 
     @staticmethod
     def get_custom_path(default_path:str) -> str: 
         path_input = input("Enter CSV file path:\n>")
         if path_input == '':
+            print("No input, going with DEFAULT path.")
             return default_path
         return path_input
     
@@ -48,25 +47,28 @@ class FileGetter():
 class FileSaver():
     @staticmethod
     def save_data(dict:dict, path: str) -> bool:
-        if not os.path.exists(path):
-            with open(path, 'w', newline='', encoding="utf-8") as csv_header:
-                csv_writer = csv.writer(csv_header)
-                csv_writer.writerow(["TOTAL", "DATE", "LOGTYPE", "SUBTYPE", "TITLE", "AMOUNT", "LOG-ID"])
-                print("Created a NEW csv file since none was found")
+        try:
+            if not os.path.exists(path):
+                with open(path, 'w', newline='', encoding="utf-8") as csv_header:
+                    csv_writer = csv.writer(csv_header)
+                    csv_writer.writerow(["TOTAL", "DATE", "LOGTYPE", "SUBTYPE", "TITLE", "AMOUNT", "LOG-ID"])
+                    print("Created a NEW csv file since none was found")
 
-        with open(path, 'a', newline='', encoding="utf-8") as csv_file:
-            csv_writer = csv.writer(csv_file)
-            csv_writer.writerow(
-                    [
-                        dict["total"],
-                        dict["date"],
-                        dict["logType"],
-                        dict["subtype"],
-                        dict["title"],
-                        dict["amount"],
-                        dict["logID"]
-                    ]
-                )
+            with open(path, 'a', newline='', encoding="utf-8") as csv_file:
+                csv_writer = csv.writer(csv_file)
+                csv_writer.writerow([
+                    dict["total"],
+                    dict["date"],
+                    dict["logType"],
+                    dict["subtype"],
+                    dict["title"],
+                    dict["amount"],
+                    dict["logID"]
+                ])
+            return True
+        except Exception as e:
+            print(f"ERROR_SAVING_FILE \'{path}\': {e}")
+            return False
 
 
 
--- PROJECT-T/main_Project-T.py
+++ PROJECT-T/main_Project-T.py
@@ -1,15 +1,16 @@
+import os
+
+from dataclasses import dataclass
+from datetime import datetime
+
 from LogFileHandling import FileGetter, FileSaver
 from LogEntry_dataclass import LogEntry
 from LogDetails import Transac, Liabili, Savings
 from debugger import debug
 
-from dataclasses import dataclass
-from datetime import datetime
-import os
-
 # GLOBAL VARIABLES
 DEFAULT_FILE_NAME:str = "AUDIT_LOG.csv"
-DEFAULT_FILE_PATH:str = os.path.join(__file__[0:-len(os.path.basename(__file__))], DEFAULT_FILE_NAME)
+DEFAULT_FILE_PATH:str = os.path.join(os.path.dirname(__file__), DEFAULT_FILE_NAME)
 
 ''' NOTES: CREATE A SINGLE LIST THAT IS SAVED, AND HAVE SPECIFIC FUNCTIONS 
     TO SEGREGATE EACH ENTRY INTO THEIR OWN RESPECTIVE LOG TYPES AND SUBTYPE 
@@ -23,37 +24,15 @@ class Auditing(LogEntry):
     currLoglist:list = []
 
     def __init__(self) -> None:
-        self.startup()
+        self.__startup()
 
     def __str__(self) -> str:
         return str(self.total)
 
-
-    def startup(self) -> None:
+    def __startup(self) -> None:
         self.check_date_change()
         Auditing.mainLogList = FileGetter.fetch_saved_database(DEFAULT_FILE_PATH)
         Auditing.currLoglist = self.get_curr_list()
-
-
-    def add_entry(self) -> None:
-        entry = LogEntry(
-                        total=self.total,
-                        date=self.date,
-                        logType=self.logType,
-                        subtype=self.subtype,
-                        title=self.title,
-                        amount=self.amount,
-                        logID=self.logID
-                        )
-
-        Auditing.mainLogList.append(entry)
-
-        if Auditing.mainLogList[-1].date == entry.date:
-            Auditing.currLoglist.append(entry)
-        else:
-            debug("Date has changed.")
-            self.set_changed_date()
-
 
     def get_curr_list(self) -> list:
         currentList = []
@@ -62,114 +41,125 @@ class Auditing(LogEntry):
                 currentList.append(obj)
         return currentList
     
+    def get_total_entry_count(self) -> int:
+        return len(Auditing.mainLogList) + 1
+    
     def check_date_change(self) -> None:
-        tempDate = datetime.now().strftime('%d-%m-%Y')
-        if not self.date == tempDate:
-            self.date = tempDate
+        currentDate = datetime.now().strftime('%d-%m-%Y')
+        if self.date != currentDate:
+            self.date = currentDate
 
     def set_changed_date(self) -> None:
         self.date = datetime.now().strftime("%d-%m-%Y")
 
+    def create_entry(self) -> object:
+        ''' Takes in user input to dynamically and automatically make entry data details '''
 
+        # FUNCTION CALLS
+        self.__init_fetch_date()
+        self.__init_fetch_entry_details()
+        self.__init_fetch_amount()
+        self.__init_fetch_total_count()
+        self.__init_create_ID()
+
+        self.__append_entry()
+        FileSaver.save_data(self.__dict__, DEFAULT_FILE_PATH)
+        
+        return self
     
 
-    def create_entry(self):
-        # Auditing.temp_fetch_mainLog()
+    def __init_fetch_date(self) -> None:
+        self.set_changed_date()
 
-        def init_fetch_date() -> None:
-            self.date = datetime.now().strftime('%d-%m-%Y')
+    def __init_fetch_entry_details(self) -> None:
+        '''Fetches info for LOGTYPE, Log SUBTYPE, and TITLE'''
 
-        def init_fetch_entry_details() -> None:
-            ''' Fetches info for LOGTYPE, Log SUBTYPE, and TITLE '''
+        user_input = None
+        while user_input not in ['A','B','C']:
+            user_input = input("""Enter Log Type:\n\tA. \'Transactions\'\n\tB. \'Liabilities\'\n\tC. \'Savings\'\n  > """).upper()
 
-            def add_title_count(duplicateTitle) -> None:
-                ''' Adds an increasing number to a duplicate/generic title '''
-
-                if " " in duplicateTitle and (duplicateTitle[-1]):
-                    duplicateTitle = duplicateTitle.split(" ")
-                    try:
-                        duplicateTitle[-1] = str(int(duplicateTitle[-1]) + 1)
-                    except ValueError or BaseException as e:
-                        print(f"TITLE_ADDITION_ERROR:\n\t{e}")
-                    finally: 
-                        self.title = " ".join(duplicateTitle)
-                else:
-                    self.title = "{} 1".format(duplicateTitle)
-
-
-            def check_generic_title() -> None:
-                genericTitles = [ "Pamasahe", "Found", "Lost", "Deposit", "Withdrawal", "Random Magic Sorcery" ]
-                if self.title in genericTitles and not " " in self.title:
-                    add_title_count(self.title)
-
-            def check_title_duplicates() -> None:
-                # excludedTitles = [ "Loaned", "Owed"]
-                for obj in Auditing.currLoglist:
-                    if self.title == obj.title:
-                        debug(f"{obj = }")
-                        debug(f"{obj.logID = }")
-                        debug(f"{self.title = } == {obj.title = }, ")
-                        print(f"Duplicate title \'{self.title}\' found, adding...")
-                        add_title_count(self.title)
+            match user_input:
+                case 'A':
+                    Transac.fetch_entry_details()
+                    self.logType = Transac.logType
+                    self.subtype = Transac.subtype
+                    self.title   = input("Input Log Title: ").strip()
+                case 'B':
+                    Liabili.fetch_entry_details()
+                    self.logType = Liabili.logType
+                    self.subtype = Liabili.subtype
+                    self.title   = Liabili.titleChoice
+                case 'C':
+                    Savings.fetch_entry_details()
+                    self.logType = Savings.logType
+                    self.subtype = Savings.subtype
+                    self.title   = Savings.titleChoice
+                case _:
+                    print(f"INPUT_ERROR: \'{user_input}\' is not part of the options.\n")
+                    continue
+        self.__check_generic_title()
+        self.__check_title_duplicates()
 
 
-            # Start of fetch details function 
-            user_input = None
+    def __init_fetch_amount(self) -> None:
+        self.__amount = 0
+        while self.amount == 0:
+            try: self.amount = abs(float(input("Input Transaction Amount: ")))
+            except ValueError as e: print(f"NaN_ERROR: {e}")
 
-            while user_input not in ['A','B','C']:
-                user_input = input("""Enter Log Type:\n\tA. \'Transactions\'\n\tB. \'Liabilities\'\n\tC. \'Savings\'\n  > """).upper()
+    def __init_fetch_total_count(self) -> None:
+        '''Fetches the total count of log Entries in the dynamic main list'''
+        self.total = self.get_total_entry_count()
 
-                match user_input:
-                    case 'A':
-                        Transac.fetch_entry_details()
-                        self.logType = Transac.logType
-                        self.subtype = Transac.subtype
-                        self.title   = input("Input Log Title: ").strip()
-                    case 'B':
-                        Liabili.fetch_entry_details()
-                        self.logType = Liabili.logType
-                        self.subtype = Liabili.subtype
-                        self.title   = Liabili.titleChoice
-                    case 'C':
-                        Savings.fetch_entry_details()
-                        self.logType = Savings.logType
-                        self.subtype = Savings.subtype
-                        self.title   = Savings.titleChoice
-                    case _:
-                        print(f"INPUT_ERROR: \'{user_input}\' is not part of the options.\n")
-                        continue
+    def __init_create_ID(self) -> None:
+        '''Creates a unique ID'''
+        self.logID = f"{self.total}//{self.logType}//{self.subtype}//{self.date}"
 
-                check_generic_title()
-                check_title_duplicates()
-            # END OF INIT_FETCH_ENTRY_DETAILS
+    def __add_title_count(self, duplicateTitle) -> None:
+        '''Adds an increasing number to a duplicate/generic title'''
+
+        if " " in duplicateTitle:
+            parts = duplicateTitle.split(" ")
+            try:
+                parts[-1] = str(int(parts[-1]) + 1)
+            except ValueError or BaseException as e:
+                print(f"TITLE_ADDITION_ERROR:\n\t{e}")
+            finally: 
+                self.title = " ".join(parts)
+        else:
+            self.title = "{} 1".format(duplicateTitle)
 
 
-        def init_fetch_amount() -> None:
-            self.amount = 0
-            while self.amount == 0:
-                try: self.amount = abs(float(input("Input Transaction Amount: ")))
-                except ValueError as e: print(f"NaN_ERROR: {e}")
+    def __check_generic_title(self) -> None:
+        genericTitles = [ "Pamasahe", "Found", "Lost", "Deposit", "Withdrawal", "Random Magic Sorcery" ]
+        if self.title in genericTitles and not " " in self.title:
+            self.__add_title_count(self.title)
 
-        def init_fetch_total_count() -> None:
-            self.total = len(Auditing.mainLogList) + 1
+    def __check_title_duplicates(self) -> None:
+        excludedTitles= ["Loaned", "Owed"]
+        for obj in Auditing.currLoglist:
+            if self.title == obj.title and self.title not in excludedTitles:
+                print(f"Duplicate title \'{self.title}\' found, adding...")
+                self.__add_title_count(self.title)
 
-        def init_create_ID() -> None:
-            self.logID = f"{self.total}//{self.logType}//{self.subtype}//{self.date}"
+    def __append_entry(self) -> None:
+        ''' Appends created entry'''
+        entry = LogEntry(
+            total=self.total,
+            date=self.date,
+            logType=self.logType,
+            subtype=self.subtype,
+            title=self.title,
+            amount=self.amount,
+            logID=self.logID
+        )
+        Auditing.mainLogList.append(entry)
 
-        
-        # FUNCTION CALLS
-        init_fetch_date()
-        init_fetch_entry_details()
-        init_fetch_amount()
-        init_fetch_total_count()
-        init_create_ID()
-
-        self.add_entry()
-        FileSaver.save_data(self.__dict__, DEFAULT_FILE_PATH)
-
-        return self
-    # END OF CREATE_ENTRY
-
+        if Auditing.mainLogList[-1].date == entry.date:
+            Auditing.currLoglist.append(entry)
+        else:
+            debug("Date has changed.")
+            self.set_changed_date()
 
     @classmethod
     def display_all_entries(cls) -> bool:
@@ -177,29 +167,12 @@ class Auditing(LogEntry):
             for obj in cls.mainLogList:
                 print(f"{obj}")
 
-    @classmethod
-    def get_total_entry_count(cls) -> int:
-        return len(cls.mainLogList) + 1
-
-    @classmethod
-    def get_main_list(cls) -> list:
-        return cls.mainLogList
-    
-
 
 
 if __name__ == '__main__':
-    prog_input:str = ''
-
     audit = Auditing()
-
-    while prog_input not in ['y']:
+    while True: 
         audit.create_entry()
-
-        prog_input = input("exit? [y/n]: ").lower()    
-
-        debug()
-        debug(f"{Auditing.mainLogList = }")
-        debug()
         audit.display_all_entries()
-        debug("end.")
+        if input("exit? [y/n]: ").lower() == 'y':
+            break
